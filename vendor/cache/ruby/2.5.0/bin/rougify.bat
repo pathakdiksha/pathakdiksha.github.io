7f795657bf3ccb5134645c97e2c5bad62d2ecf1e
pathakdiksha <dikshapathak.95@gmail.com>
2019-10-23 23:19:55 +0530

    proper mvp

---
+++ vendor/cache/ruby/2.5.0/bin/rougify.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\Ruby25-x64\bin\ruby.exe" "C:/Users/dp056738/desktop/pathakdiksha.github.io/vendor/cache/ruby/2.5.0/bin/rougify" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\Ruby25-x64\bin\ruby.exe" "%~dpn0" %*
